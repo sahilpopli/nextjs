a0d1deb4d6b582da017866058414b2095dd6c120
Brahma <belikebrahma@gmail.com>
2022-09-09 21:50:11 +0530

    Hello Next

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Brahma!</a>
         </h1>
 
         <p className={styles.description}>
@@ -54,14 +54,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://brah.ma"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by Brahma
+          
         </a>
       </footer>
     </div>

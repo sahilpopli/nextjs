0a43cc1698e2136aa7f65cd67eb03b519253d369
Brahma <arisingpopli@gmail.com>
2022-09-10 02:50:41 +0530

    /link

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <link to="/about">Brahma!</a>
+          Welcome to <link to="/about">Brahma!</link>
         </h1>
 
         <p className={styles.description}>
